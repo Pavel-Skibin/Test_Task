62546c13ff45069f6395effa8b68c9a6c5470a91
Pavel-Skibin <SkibinPS05@gmail.com>
2025-04-19 11:05:53 +0300

    Initial commit: add Spring Boot project

---
+++ HolidayPayTool/src/main/java/com/nahap/HolidayPayTool/HolidayPayToolApplication.java
@@ -0,0 +1,13 @@
+package com.nahap.HolidayPayTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayPayToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayPayToolApplication.class, args);
+	}
+
+}
---
+++ HolidayPayTool/src/test/java/com/nahap/HolidayPayTool/HolidayPayToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nahap.HolidayPayTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayPayToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
